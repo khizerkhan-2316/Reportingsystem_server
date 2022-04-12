f83ff041cb1fad40780abf1cb628d088f1a39252
khizerkhan-2316 <khizer759@gmail.com>
2022-04-12 03:03:01 +0200

    fixing deployment issues

--- src/server.js
+++ src/server.js
@@ -7,8 +7,8 @@ const passport = require('passport');
 
 require('dotenv').config();
 
-//const { DB } = require('./config/index.js');
-const { DB } = require('./LocalConfig.js');
+const { DB } = require('./config/index.js');
+//const { DB } = require('./LocalConfig.js');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
